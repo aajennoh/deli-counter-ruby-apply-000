bc8ad2a1915969309e4702afe1f3090eb0db0624
JENNIFER OH <ohjennifer.90@gmail.com>
2019-03-12 16:38:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,20 @@
 def line(katz_deli)
-  if katz_deli.empty?
+  if katz_deli.empty? #boolean to determine whether or not statement is true
     puts "The line is currently empty."
   else
-    new_array = [ ]
-    #.each won't modify the original array, so you have to push the values you create into a new array
-    number = 1
+    new_array = [ ] #.each won't modify the original array, so you have to push the values you create into a new array
+    number = 1 #counter starting at 1
     katz_deli.each do |customer|
-      new_array << "#{number}. #{customer}"
-      number += 1
+      new_array << "#{number}. #{customer}" #for each loop, set the customer and associated number into the new array. basically repeat until last customer
+      number += 1 #for each loop, increase counter by 1 and the next loop will have the next number with the next customer
     end
     puts "The line is currently: #{new_array.join(" ")}"
+    #.join will exclude all of the fluff between each new_array
   end
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli << name #adding the customer to the end of the line
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
